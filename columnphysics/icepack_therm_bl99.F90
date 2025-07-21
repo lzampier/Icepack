0a1e1eb90bfe1bc7a39c0772259abe8164da8925
Lorenzo Zampieri <lorenzo.zampieri992@gmail.com>
2025-07-21 21:58:16 +0200

    Fix indentation

--- columnphysics/icepack_therm_bl99.F90
+++ columnphysics/icepack_therm_bl99.F90
@@ -923,70 +923,70 @@
 !
 ! Adjust thermal conductivity at interfaces to account for
 ! unresolved horizontal conduction of heat. This parameterization
-! generates from the results in Zampieri et al. (2023)
+! generates from the results in Zampieri et al. (2024)
 ! DOI: https://doi.org/10.1029/2023GL106760
 !
 ! Author: Lorenzo Zampieri, ECMWF (previously NCAR)
 !
-subroutine adjust_conductivity_horizontal_conduction (kh, fcondbot)
-    implicit none
+      subroutine adjust_conductivity_horizontal_conduction (kh, fcondbot)
+         implicit none
 
-    !-----------------------------------------------------------------
-    ! Arguments
-    !-----------------------------------------------------------------
-    real (kind=dbl_kind), dimension(:), intent(inout) :: kh          ! conductivity at interfaces (W m-2 K-1)
-    real (kind=dbl_kind), intent(in)                  :: fcondbot    ! downward conductive flux at ice bottom (W m-2)
+         !-----------------------------------------------------------------
+         ! Arguments
+         !-----------------------------------------------------------------
+         real (kind=dbl_kind), dimension(:), intent(inout) :: kh          ! conductivity at interfaces (W m-2 K-1)
+         real (kind=dbl_kind), intent(in)                  :: fcondbot    ! downward conductive flux at ice bottom (W m-2)
 
-    !-----------------------------------------------------------------
-    ! Local constants and variables
-    !-----------------------------------------------------------------
-    real (kind=dbl_kind), parameter :: origin_x = -c2                ! x-coordinate of lines origin
-    real (kind=dbl_kind), parameter :: origin_y = 0.17_dbl_kind      ! y-coordinate of lines origin
+         !-----------------------------------------------------------------
+         ! Local constants and variables
+         !-----------------------------------------------------------------
+         real (kind=dbl_kind), parameter :: origin_x = -c2                ! x-coordinate of lines origin
+         real (kind=dbl_kind), parameter :: origin_y = 0.17_dbl_kind      ! y-coordinate of lines origin
 
-    integer (kind=int_kind)         :: k                             ! vertical index
-    real (kind=dbl_kind)            :: fit_slope                     ! slope of correction line
-    real (kind=dbl_kind)            :: fit_intercept                 ! intercept of correction line
-    real (kind=dbl_kind)            :: delta_fcondbot                ! adjustment to fcondbot
-    real (kind=dbl_kind)            :: threshold                     ! threshold value for correction
-    real (kind=dbl_kind)            :: kh_correction_factor          ! correction multiplier applied to kh
+         integer (kind=int_kind)         :: k                             ! vertical index
+         real (kind=dbl_kind)            :: fit_slope                     ! slope of correction line
+         real (kind=dbl_kind)            :: fit_intercept                 ! intercept of correction line
+         real (kind=dbl_kind)            :: delta_fcondbot                ! adjustment to fcondbot
+         real (kind=dbl_kind)            :: threshold                     ! threshold value for correction
+         real (kind=dbl_kind)            :: kh_correction_factor          ! correction multiplier applied to kh
 
-    logical (kind=log_kind), parameter :: debug_mode = .false.       ! debug mode flag (local debug)
+         logical (kind=log_kind), parameter :: debug_mode = .false.       ! debug mode flag (local debug)
 
-    character(len=*), parameter     :: subname = '(adjust_conductivity_horizontal_conduction)'
+         character(len=*), parameter     :: subname = '(adjust_conductivity_horizontal_conduction)'
 
-    !-----------------------------------------------------------------
-    ! Compute slope and intercept of correction line
-    !-----------------------------------------------------------------
-    call smooth_interpolate(ksno, fit_slope)
-    fit_intercept = origin_y - fit_slope * origin_x
+         !-----------------------------------------------------------------
+         ! Compute slope and intercept of correction line
+         !-----------------------------------------------------------------
+         call smooth_interpolate(ksno, fit_slope)
+         fit_intercept = origin_y - fit_slope * origin_x
 
-    !-----------------------------------------------------------------
-    ! Compute correction to fcondbot (capped at zero)
-    !-----------------------------------------------------------------
-    delta_fcondbot = min(fit_slope * fcondbot + fit_intercept, c0)
+         !-----------------------------------------------------------------
+         ! Compute correction to fcondbot (capped at zero)
+         !-----------------------------------------------------------------
+         delta_fcondbot = min(fit_slope * fcondbot + fit_intercept, c0)
 
-    !-----------------------------------------------------------------
-    ! Determine correction factor for kh
-    !-----------------------------------------------------------------
-    threshold = -fit_intercept / fit_slope
-    if (fcondbot < threshold) then
-        kh_correction_factor = 1.0_dbl_kind + delta_fcondbot / fcondbot
-        kh(:) = kh(:) * kh_correction_factor
-    else
-        kh_correction_factor = 1.0_dbl_kind  ! no correction applied
-    endif
+         !-----------------------------------------------------------------
+         ! Determine correction factor for kh
+         !-----------------------------------------------------------------
+         threshold = -fit_intercept / fit_slope
+         if (fcondbot < threshold) then
+            kh_correction_factor = 1.0_dbl_kind + delta_fcondbot / fcondbot
+            kh(:) = kh(:) * kh_correction_factor 
+         else
+            kh_correction_factor = 1.0_dbl_kind  ! no correction applied
+         endif
 
-    !-----------------------------------------------------------------
-    ! Optional debug output (needs to be removed before merging)
-    !-----------------------------------------------------------------
-    if (debug_mode) then
-        print *, 'DEBUG:', subname
-        print *, 'slope=', fit_slope, ' intercept=', fit_intercept
-        print *, 'fcondbot=', fcondbot, ' threshold=', threshold
-        print *, 'correction factor applied to kh =', kh_correction_factor
-    endif
+         !-----------------------------------------------------------------
+         ! Optional debug output (needs to be removed before merging)
+         !-----------------------------------------------------------------
+         if (debug_mode) then
+            print *, 'DEBUG:', subname
+            print *, 'slope=', fit_slope, ' intercept=', fit_intercept
+            print *, 'fcondbot=', fcondbot, ' threshold=', threshold
+            print *, 'correction factor applied to kh =', kh_correction_factor
+         endif
 
-end subroutine adjust_conductivity_horizontal_conduction
+      end subroutine adjust_conductivity_horizontal_conduction
 
 !=======================================================================
 !
@@ -1576,7 +1576,7 @@ end subroutine adjust_conductivity_horizontal_conduction
 !
 !=======================================================================
       subroutine smooth_interpolate(x_input, y_output)
-      use icepack_kinds, only: dbl_kind
+
       implicit none
 
       !-----------------------------------------------------------------
